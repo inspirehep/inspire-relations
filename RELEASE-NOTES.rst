136655c90aa91c62374aeb9862a8e0f206be0013
Samuele Kaplun <samuele.kaplun@cern.ch>
2016-06-15 11:01:30 +0200

    relations: 1st commit
    Signed-off-by: Samuele Kaplun <samuele.kaplun@cern.ch>

---
+++ RELEASE-NOTES.rst
@@ -0,0 +1,36 @@
+==========================
+ Inspire-Relations v0.1.0
+==========================
+
+Inspire-Relations v0.1.0 was released on TBD, 2016.
+
+About
+-----
+
+Invenio module to integrate Neo4J graph database and handle relations across records.
+
+*This is an experimental developer preview release.*
+
+What's new
+----------
+
+- Initial public release.
+
+Installation
+------------
+
+   $ pip install inspire-relations==0.1.0
+
+Documentation
+-------------
+
+   http://pythonhosted.org/inspire-relations/
+
+Happy hacking and thanks for flying Inspire-Relations.
+
+| Invenio Development Team
+|   Email: info@invenio-software.org
+|   IRC: #invenio on irc.freenode.net
+|   Twitter: http://twitter.com/inveniosoftware
+|   GitHub: https://github.com/inspirehep/inspire-relations
+|   URL: http://invenio-software.org
---
+++ inspire_relations/ext.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2016 CERN.
+#
+# INSPIRE is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Invenio module to integrate Neo4J graph database and handle relations across records."""
+
+from __future__ import absolute_import, print_function
+
+from flask_babelex import gettext as _
+
+from .views import blueprint
+
+
+class InspireRelations(object):
+    """Inspire-Relations extension."""
+
+    def __init__(self, app=None):
+        """Extension initialization."""
+        # TODO: This is an example of translation string with comment. Please
+        # remove it.
+        # NOTE: This is a note to a translator.
+        _('A translation string')
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Flask application initialization."""
+        self.init_config(app)
+        app.register_blueprint(blueprint)
+        app.extensions['inspire-relations'] = self
+
+    def init_config(self, app):
+        """Initialize configuration."""
+        app.config.setdefault(
+            "INSPIRE_RELATIONS_BASE_TEMPLATE",
+            app.config.get("BASE_TEMPLATE",
+                           "inspire_relations/base.html"))
